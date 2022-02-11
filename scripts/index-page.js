068228472d90cb2881bd9d57573eecc2377ef748
Raquel Pantin <raquelpantin@Raquels-MBP.attlocal.net>
2022-02-11 11:30:31 -0500

    added comments to DOM

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -21,63 +21,41 @@ const userComments = [
 
 console.log(userComments);
 
-function addElement(tag, str) {
-  const newElement = document.createElement(tag);
-  newElement.setAttribute("class", str);
-  const mainContainer = document.querySelector(".user-comment__all");
-  mainContainer.appendChild(newElement);
+const commentSection = document.querySelector(".user-comment__all");
+
+function displayComment(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let commentArticle = document.createElement("article");
+    commentArticle.classList.add("user-comment__container");
+    commentSection.appendChild(commentArticle);
+
+    let imgDiv = document.createElement("div");
+    imgDiv.classList.add("no-avatar__image");
+    commentArticle.appendChild(imgDiv);
+
+    let commentCard = document.createElement("div");
+    commentCard.classList.add("user-comment__card");
+    commentArticle.appendChild(commentCard);
+
+    let commentList = document.createElement("ul");
+    commentList.classList.add("user-comment__list");
+    commentCard.appendChild(commentList);
+
+    let commentListItem = document.createElement("li");
+    commentListItem.classList.add("user-comment__list-item");
+    commentListItem.innerHTML = arr[i].name;
+    commentList.appendChild(commentListItem);
+
+    let commentListDate = document.createElement("li");
+    commentListDate.classList.add("user-comment__list-item--date");
+    commentListDate.innerHTML = arr[i].date;
+    commentList.appendChild(commentListDate);
+
+    let comment = document.createElement("p");
+    comment.classList.add("user-comment__input");
+    comment.innerHTML = arr[i].comment;
+    commentCard.appendChild(comment);
+  }
 }
 
-function addList(tag, str, str2) {
-  const newList = document.createElement(tag);
-  newList.setAttribute("class", str);
-  const parentContainer = document.querySelector("." + str2);
-  parentContainer.appendChild(newList);
-}
-
-addElement("hr", "divider-first");
-addElement("div", "user-comment__container");
-addElement("div", "no-avatar__image");
-addElement("div", "user-comment__card");
-addList("ul", "user-comment__list", "user-comment__card");
-
-addElement("hr", "divider-first");
-addElement("div", "user-comment__container");
-addElement("div", "no-avatar__image");
-addElement("div", "user-comment__card");
-addList("ul", "user-comment__list", "user-comment__card");
-
-addElement("hr", "divider-first");
-addElement("div", "user-comment__container");
-addElement("div", "no-avatar__image");
-addElement("div", "user-comment__card");
-addList("ul", "user-comment__list", "user-comment__card");
-
-// addList("li", "user-comment__list-item", "user-comment__card");
-
-const unorderedList = document.querySelector(".user-comment__list");
-const userCommentCard = document.querySelector(".user-comment__card");
-
-for (let index = 0; index < userComments.length; index++) {
-  let userNames = document.createElement("li");
-  userNames.setAttribute("class", "user-comment__list-item");
-  userNames.innerText = userComments[index].name;
-  unorderedList.appendChild(userNames);
-  console.log(userComments[index].name);
-}
-
-for (let index = 0; index < userComments.length; index++) {
-  let commentDate = document.createElement("li");
-  commentDate.setAttribute("class", "user-comment__list-item--date");
-  commentDate.innerText = userComments[index].date;
-  unorderedList.appendChild(commentDate);
-  console.log(userComments[index].date);
-}
-
-for (let index = 0; index < userComments.length; index++) {
-  let comment = document.createElement("p");
-  comment.setAttribute("class", "user-comment__input");
-  comment.innerText = userComments[index].comment;
-  userCommentCard.appendChild(comment);
-  console.log(userComments[index].comment);
-}
+displayComment(userComments);
