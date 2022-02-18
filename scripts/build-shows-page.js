6aedd19d64ab94432ab16dadcbcf93d634bd845f
Raquel Pantin <raquel.pantin@gmail.com>
2022-02-17 20:23:43 -0500

    used API to input show listing data

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,55 +1,68 @@
-const showListing = [
-  {
-    dateTitle: "DATE",
-    date: "Mon Sept 06 2021",
-    venueTitle: "VENUE",
-    venue: "Ronald Lane",
-    locationTitle: "LOCATION",
-    location: "San Francisco, CA",
-  },
-  {
-    dateTitle: "DATE",
-    date: "Tue Sept 21 2021",
-    venueTitle: "VENUE",
-    venue: "Pier 3 East",
-    locationTitle: "LOCATION",
-    location: "San Francisco, CA",
-  },
-  {
-    dateTitle: "DATE",
-    date: "Fri Oct 15 2021",
-    venueTitle: "VENUE",
-    venue: "View Lounge",
-    locationTitle: "LOCATION",
-    location: "San Francisco, CA",
-  },
-  {
-    dateTitle: "DATE",
-    date: "Sat Nov 06 2021",
-    venueTitle: "VENUE",
-    venue: "Hyatt Agency",
-    locationTitle: "LOCATION",
-    location: "San Francisco, CA",
-  },
-  {
-    dateTitle: "DATE",
-    date: "Fri Nov 26 2021",
-    venueTitle: "VENUE",
-    venue: "Moscow Center",
-    locationTitle: "LOCATION",
-    location: "San Francisco, CA",
-  },
-  {
-    dateTitle: "DATE",
-    date: "Wed Dec 15 2021",
-    venueTitle: "VENUE",
-    venue: "Press Club",
-    locationTitle: "LOCATION",
-    location: "San Francisco, CA",
-  },
-];
+// const showListing = [
+//   {
+//     dateTitle: "DATE",
+//     date: "Mon Sept 06 2021",
+//     venueTitle: "VENUE",
+//     venue: "Ronald Lane",
+//     locationTitle: "LOCATION",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     dateTitle: "DATE",
+//     date: "Tue Sept 21 2021",
+//     venueTitle: "VENUE",
+//     venue: "Pier 3 East",
+//     locationTitle: "LOCATION",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     dateTitle: "DATE",
+//     date: "Fri Oct 15 2021",
+//     venueTitle: "VENUE",
+//     venue: "View Lounge",
+//     locationTitle: "LOCATION",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     dateTitle: "DATE",
+//     date: "Sat Nov 06 2021",
+//     venueTitle: "VENUE",
+//     venue: "Hyatt Agency",
+//     locationTitle: "LOCATION",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     dateTitle: "DATE",
+//     date: "Fri Nov 26 2021",
+//     venueTitle: "VENUE",
+//     venue: "Moscow Center",
+//     locationTitle: "LOCATION",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     dateTitle: "DATE",
+//     date: "Wed Dec 15 2021",
+//     venueTitle: "VENUE",
+//     venue: "Press Club",
+//     locationTitle: "LOCATION",
+//     location: "San Francisco, CA",
+//   },
+// ];
 
-console.log(showListing);
+// console.log(showListing);
+
+const apiKey = "1e2adea7-0680-4c90-a680-774c62dfd9ca";
+
+const commentURL =
+  "https://project-1-api.herokuapp.com/showdates?api_key=1e2adea7-0680-4c90-a680-774c62dfd9ca";
+
+axios.get(commentURL).then((response) => {
+  console.log(response.data);
+  let showArray = response.data;
+  showArray.forEach((shows) => {
+    displayShows(shows);
+  });
+});
 
 const showMain = document.querySelector(".shows__main");
 
@@ -76,59 +89,55 @@ function displayTitles() {
 
 displayTitles();
 
-function displayShows(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let showSection = document.createElement("section");
-    showSection.classList.add("shows__individual");
-    showMain.appendChild(showSection);
+function displayShows(shows) {
+  let showSection = document.createElement("section");
+  showSection.classList.add("shows__individual");
+  showMain.appendChild(showSection);
 
-    let showList = document.createElement("ul");
-    showList.classList.add("shows__list");
-    showSection.appendChild(showList);
+  let showList = document.createElement("ul");
+  showList.classList.add("shows__list");
+  showSection.appendChild(showList);
 
-    let showListDate = document.createElement("li");
-    showListDate.classList.add("shows__list-date");
-    showListDate.innerText = arr[i].dateTitle;
-    showList.appendChild(showListDate);
+  let showListDate = document.createElement("li");
+  showListDate.classList.add("shows__list-date");
+  showListDate.innerText = "DATE";
+  showList.appendChild(showListDate);
 
-    let showListDateInfo = document.createElement("li");
-    showListDateInfo.classList.add("shows__list-date--info");
-    showListDateInfo.innerText = arr[i].date;
-    showList.appendChild(showListDateInfo);
+  let showListDateInfo = document.createElement("li");
+  showListDateInfo.classList.add("shows__list-date--info");
+  showListDateInfo.innerText = shows.date;
+  showList.appendChild(showListDateInfo);
 
-    let showListVenue = document.createElement("li");
-    showListVenue.classList.add("shows__list-venue");
-    showListVenue.innerText = arr[i].venueTitle;
-    showList.appendChild(showListVenue);
+  let showListVenue = document.createElement("li");
+  showListVenue.classList.add("shows__list-venue");
+  showListVenue.innerText = "VENUE";
+  showList.appendChild(showListVenue);
 
-    let showListVenueInfo = document.createElement("li");
-    showListVenueInfo.classList.add("shows__list-venue--info");
-    showListVenueInfo.innerText = arr[i].venue;
-    showList.appendChild(showListVenueInfo);
+  let showListVenueInfo = document.createElement("li");
+  showListVenueInfo.classList.add("shows__list-venue--info");
+  showListVenueInfo.innerText = shows.place;
+  showList.appendChild(showListVenueInfo);
 
-    let showListLocation = document.createElement("li");
-    showListLocation.classList.add("shows__list-location");
-    showListLocation.innerText = arr[i].locationTitle;
-    showList.appendChild(showListLocation);
+  let showListLocation = document.createElement("li");
+  showListLocation.classList.add("shows__list-location");
+  showListLocation.innerText = "LOCATION";
+  showList.appendChild(showListLocation);
 
-    let showListLocationInfo = document.createElement("li");
-    showListLocationInfo.classList.add("shows__list-location--info");
-    showListLocationInfo.innerText = arr[i].location;
-    showList.appendChild(showListLocationInfo);
+  let showListLocationInfo = document.createElement("li");
+  showListLocationInfo.classList.add("shows__list-location--info");
+  showListLocationInfo.innerText = shows.location;
+  showList.appendChild(showListLocationInfo);
 
-    let showButton = document.createElement("a");
-    showButton.classList.add("shows__button");
-    showButton.innerText = "BUY TICKETS";
-    showButton.setAttribute("href", "./shows.html");
-    showSection.appendChild(showButton);
+  let showButton = document.createElement("a");
+  showButton.classList.add("shows__button");
+  showButton.innerText = "BUY TICKETS";
+  showButton.setAttribute("href", "./shows.html");
+  showSection.appendChild(showButton);
 
-    let showDivider = document.createElement("div");
-    showDivider.classList.add("shows__divider");
-    showMain.appendChild(showDivider);
-  }
+  let showDivider = document.createElement("div");
+  showDivider.classList.add("shows__divider");
+  showMain.appendChild(showDivider);
 }
-
-displayShows(showListing);
 
 const tableRow = document.querySelectorAll(".shows__individual");
 
@@ -136,5 +145,6 @@ for (let i = 0; i < tableRow.length; i++) {
   tableRow[i].addEventListener("dblclick", function (e) {
     e.preventDefault();
     tableRow[i].classList.toggle("shows__individual-active");
+    console.log("clicked");
   });
 }
