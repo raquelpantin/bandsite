d7f7c21613af629c394953055ecd972e0e7a5a60
Raquel Pantin <raquel.pantin@gmail.com>
2022-02-20 16:27:08 -0500

    updated bio page js for submission form error

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,20 +3,6 @@ const apiKey = "1e2adea7-0680-4c90-a680-774c62dfd9ca";
 const commentURL =
   "https://project-1-api.herokuapp.com/comments?api_key=1e2adea7-0680-4c90-a680-774c62dfd9ca";
 
-// axios
-//   .get(commentURL)
-//   .then((response) => {
-//     console.log(response);
-//     let userArray = response.data;
-
-//     userArray.forEach((com) => {
-//       displayComment(com);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 const commentSection = document.querySelector(".user-comment__all");
 
 function displayComment(com) {
@@ -93,6 +79,15 @@ commentForm.addEventListener("submit", (event) => {
   let nameField = document.querySelector(".comment__name");
   let commentField = document.querySelector(".comment__text");
 
+  if (!commentName) {
+    nameField.classList.add("comment__error");
+    console.log("no user name added");
+  }
+  if (!commentUser) {
+    commentField.classList.add("comment__error");
+    console.log("no comment from user");
+  }
+
   if (commentName && commentUser) {
     commentSection.innerText = "";
     axios
@@ -107,8 +102,8 @@ commentForm.addEventListener("submit", (event) => {
             });
             commentArray.forEach((comment) => {
               displayComment(comment);
-              nameField.value = "";
-              commentField.value = "";
+              nameField.classList.remove("comment__error");
+              commentField.classList.remove("comment__error");
             });
           })
           .catch((err) => {
@@ -118,52 +113,6 @@ commentForm.addEventListener("submit", (event) => {
       .catch((err) => {
         console.log(err);
       });
-    form.reset();
+    commentForm.reset();
   }
 });
-
-// commentForm.addEventListener("submit", (event) => {
-//   event.preventDefault();
-
-//   let commentName = event.target.name.value;
-//   let today = new Date();
-//   let commentDate =
-//     ("0" + (today.getMonth() + 1)).slice(-2) +
-//     "/" +
-//     today.getDate() +
-//     "/" +
-//     today.getFullYear();
-//   let commentUser = event.target.text.value;
-
-//   let nameField = document.querySelector(".comment__name");
-//   let commentField = document.querySelector(".comment__text");
-
-//   if (commentName && commentDate && commentUser) {
-//     commentSection.innerText = "";
-//     userComments.unshift({
-//       name: commentName,
-//       date: commentDate,
-//       comment: commentUser,
-//     });
-
-//     displayComment(userComments);
-//     let clearName = document.querySelector(".comment__name");
-//     clearName.value = " ";
-
-//     let clearComment = document.querySelector(".comment__text");
-//     clearComment.value = " ";
-
-//     nameField.classList.remove("comment__error");
-//   }
-//   if (!commentName) {
-//     nameField.classList.add("comment__error");
-//   }
-//   if (!commentUser) {
-//     commentField.classList.add("comment__error");
-//     console.log("no comment from user");
-//   }
-// });
-
-// let time = new Date(timestamp);
-// let = time.getMonth() + 1 + "/" + time.getDate() + "/" + time.getFullYear();
-// console.log(date);
